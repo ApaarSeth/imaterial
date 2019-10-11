15fed309eff6a5c97678179dd8420b2c649a138b
Renu <renu.kamboj@buildsupply.com>
2019-10-11 15:39:30 +0530

    Signed-off-by: Renu <renu.kamboj@buildsupply.com>

---
+++ src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import {
+    Component,
+    OnInit,
+  } from '@angular/core';
+
+  @Component({
+    selector: 'dashboard',
+    templateUrl: './dashboard.html',
+    styleUrls: ['../../../assets/scss/main.scss']
+  })
+  export class DashboardComponent implements OnInit {
+    isNavbarCollapsed=true;
+    constructor(
+    ) {}
+  
+    ngOnInit() {
+      
+    }
+  
+  }
+  
