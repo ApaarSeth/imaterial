7a62afd1bd29c4cff4371f732c33193ecc05ab3b
Renu <renu.kamboj@buildsupply.com>
2019-10-14 20:19:10 +0530

    Signed-off-by: Renu <renu.kamboj@buildsupply.com>

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,19 +1,20 @@
 import { Component,OnInit } from '@angular/core';
 import {NgModule} from '@angular/core';
 
+
   @Component({
     selector: 'fix-header',
     templateUrl: './header.html',
     styleUrls: ['../../../../assets/scss/main.scss']
   })
   export class HeaderLayoutModule implements OnInit {
- 
+
     constructor(
     ) {}
   
     ngOnInit() {
       
     }
-  
+
   }
   
--- src/app/shared/material-modules.ts
+++ src/app/shared/material-modules.ts
@@ -1,11 +1,19 @@
 import {NgModule} from '@angular/core';
 
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 
 @NgModule({
   exports: [
-    MatCardModule 
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule
  ]
 })
 export class MaterialModule {}
