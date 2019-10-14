e4ba0b3b7795b180d2c8395fbe0fe7512397e953
Renu <renu.kamboj@buildsupply.com>
2019-10-14 11:33:14 +0530

    Signed-off-by: Renu <renu.kamboj@buildsupply.com>

---
+++ src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component,OnInit } from '@angular/core';
+import {NgModule} from '@angular/core';
+
+  @Component({
+    selector: 'fix-header',
+    templateUrl: './header.html',
+    styleUrls: ['../../../../assets/scss/main.scss']
+  })
+  export class HeaderLayoutModule implements OnInit {
+ 
+    constructor(
+    ) {}
+  
+    ngOnInit() {
+      
+    }
+  
+  }
+  
---
+++ src/app/shared/layout/layout-module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import { HeaderLayoutModule } from './header/header.component';
+
+
+
+@NgModule({
+    imports: [
+        
+    ] ,
+    providers:[],
+    exports:[
+        HeaderLayoutModule
+    ],
+    declarations: [ 
+        HeaderLayoutModule
+    ]
+  })
+export class LayoutModule {}
