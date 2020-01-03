6cb338fe284ad99ad022d833b4c63aa2a504ef2c
Renu <renu.kamboj@buildsupply.com>
2020-01-03 17:46:28 +0530

    Signed-off-by: Renu <renu.kamboj@buildsupply.com>

--- src/app/shared/constants/configuration-constants.ts
+++ src/app/shared/constants/configuration-constants.ts
@@ -4,7 +4,8 @@ export const ConfigurationConstants = {
 };
 export const API = {
 
-    PROJECTS: (organizationId,userId) => `projects/${organizationId}/${userId}`
+    PROJECTS: (organizationId,userId) => `projects/${organizationId}/${userId}`,
+    ADDPROJECT: (organizationId,userId) => `addProject/${organizationId}/${userId}`
     //STATE: 'account/api/permitted/address/states',
     
 }
