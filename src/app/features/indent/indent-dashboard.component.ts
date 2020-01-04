e6e8cd2a40d5214a2dd840eced32d106be5f8eb7
Renu <renu.kamboj@buildsupply.com>
2020-01-04 14:13:14 +0530

    Signed-off-by: Renu <renu.kamboj@buildsupply.com>

--- src/app/features/indent/indent-dashboard.component.ts
+++ src/app/features/indent/indent-dashboard.component.ts
@@ -12,10 +12,25 @@ import { ProjectDetails, ProjetPopupData } from 'src/app/shared/models/project-d
 //import { DoubleConfirmationComponent } from 'src/app/shared/dialogs/double-confirmation/double-confirmation.component';
 
 
-// export interface DialogData {
-//   animal: string;
-//   name: string;
-// }
+export interface PeriodicElement {
+  materialName: string;
+  estimatedQty: number;
+  requiredQty: number;
+  requiredDate: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 1.0079, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 4.0026, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 6.941, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 9.0122, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 10.811, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 12.0107, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 14.0067, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 15.9994, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM', estimatedQty: 200, requiredQty: 18.9984, requiredDate: 22/2/2020},
+  {materialName: 'Steelbar 15MM',estimatedQty: 200, requiredQty: 20.1797, requiredDate: 22/2/2020},
+];
   @Component({
     selector: 'dashboard',
     templateUrl: './indent-dashboard.component.html',
@@ -26,6 +41,8 @@ import { ProjectDetails, ProjetPopupData } from 'src/app/shared/models/project-d
     searchText: string = null;
     projectId: number;
     product: any;
+    displayedColumns: string[] = ['Material Name', 'Estimated Quantity', 'Required Quantity', 'Required Date'];
+    dataSource = ELEMENT_DATA;
 
     constructor(
       //private projectService: ProjectService,
