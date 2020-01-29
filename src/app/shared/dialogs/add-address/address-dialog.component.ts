124a2a5e7f96fa04da19ed162d04c7ce0158461b
Renu <renu.kamboj@buildsupply.com>
2020-01-29 19:37:12 +0530

    Signed-off-by: Renu <renu.kamboj@buildsupply.com>

--- src/app/shared/dialogs/add-address/address-dialog.component.ts
+++ src/app/shared/dialogs/add-address/address-dialog.component.ts
@@ -1,7 +1,13 @@
 import { Component, Inject } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
-import { RfqMaterialResponse } from "../../models/RFQ/rfq-details";
+import { RfqMaterialResponse, Address } from "../../models/RFQ/rfq-details";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AddAddressService } from "../../services/add-address/add-address.service";
 
+export interface City {
+  value: string;
+  viewValue: string;
+}
 // Component for dialog box
 @Component({
   selector: "address-dialog",
@@ -12,12 +18,64 @@ import { RfqMaterialResponse } from "../../models/RFQ/rfq-details";
 export class AddAddressDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<AddAddressDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: RfqMaterialResponse
+    @Inject(MAT_DIALOG_DATA) public data: RfqMaterialResponse,
+    private formBuilder: FormBuilder,
+    private addAddressService: AddAddressService
   ) {}
+  selectAddressFrm: FormGroup;
+  newAddressForm: FormGroup;
+  address: Address;
   ngOnInit() {
     console.log("data", this.data);
+    this.formInit();
   }
+  cities: City[] = [
+    { value: "Gurgaon", viewValue: "Gurgaon" },
+    { value: "Delhi-1", viewValue: "Delhi" },
+    { value: "Karnal", viewValue: "Karnal" }
+  ];
+
+  formInit() {
+    this.selectAddressFrm = this.formBuilder.group({
+      address: []
+    });
+    console.log("form data", this.selectAddressFrm);
+
+    // new address form
+    this.newAddressForm = this.formBuilder.group({
+      addressLine1: ["", Validators.required],
+      addressLine2: ["", Validators.required],
+      pinCode: ["", Validators.required],
+      state: ["", Validators.required],
+      city: ["", Validators.required]
+    });
+
+    console.log("addresss", this.newAddressForm.value);
+  }
+
   onNoClick(): void {
+    this.data.projectAddressList = this.data.projectAddressList.map(address => {
+      address.primaryAddress = 0;
+      return address;
+    });
+    this.selectAddressFrm.value.address.primaryAddress = 1;
+    this.data.defaultAddress = this.selectAddressFrm.value.address;
+    console.log("defaultAdressUpdate", this.data);
     this.dialogRef.close();
+  }
+
+  onNoClick1(): void {
+    this.address = this.newAddressForm.value;
+    console.log("addresss", this.newAddressForm.value);
+    console.log("new object", this.address);
+    this.postAddAddress(this.address);
+  }
+
+  postAddAddress(address) {
+    this.addAddressService
+      .postAddAddress("Project", this.data.projectId, address)
+      .then(res => {
+        res.data;
+      });
   }
 }
