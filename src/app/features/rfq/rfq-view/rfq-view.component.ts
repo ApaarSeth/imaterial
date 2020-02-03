cdb6172ee6eb88f1600a723e4f4afb4035c6f8ff
mskn06 <muskaan24mj@gmail.com>
2020-02-03 18:45:38 +0530

    rfq-view page updated

--- src/app/features/rfq/rfq-view/rfq-view.component.ts
+++ src/app/features/rfq/rfq-view/rfq-view.component.ts
@@ -3,7 +3,7 @@ import { MatDialog } from "@angular/material";
 import { ViewDocumentsDialogComponent } from "src/app/shared/dialogs/view-documents/view-documents-dialog.component";
 import { RFQService } from "src/app/shared/services/rfq/rfq.service";
 import { ActivatedRoute } from '@angular/router';
-import { Rfq } from 'src/app/shared/models/RFQ/rfq-view';
+import { Rfq, Documents, TermsObj } from 'src/app/shared/models/RFQ/rfq-view';
 
 @Component({
     selector: "rfq-view",
@@ -14,6 +14,7 @@ export class RFQViewComponent implements OnInit {
     displayedColumns: string[] = ["Material Name", "Quantity", "Makes"];
     rfqDetails: Rfq = {} as Rfq;
     rfqId: number;
+    terms: TermsObj;
 
     constructor(public dialog: MatDialog,
         private route: ActivatedRoute,
@@ -35,10 +36,12 @@ export class RFQViewComponent implements OnInit {
         });
     }
 
-    openDialog1(): void {
+    openDialog1(documentsList: Documents, terms: TermsObj): void {
         if (ViewDocumentsDialogComponent) {
             const dialogRef = this.dialog.open(ViewDocumentsDialogComponent, {
-                width: "1200px"
+                width: "1200px",
+                data: { documentsList, terms }
+
             });
             dialogRef.afterClosed().subscribe(result => {
                 console.log("The dialog was closed");
