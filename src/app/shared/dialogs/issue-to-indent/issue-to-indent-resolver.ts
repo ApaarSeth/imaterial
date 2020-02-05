f96fb816e64309a799b170f9b36dbf805dfa0397
Apaar <apaar.seth@buildsupply.com>
2020-02-05 17:44:32 +0530

    po flow

--- src/app/shared/dialogs/issue-to-indent/issue-to-indent-resolver.ts
+++ src/app/shared/dialogs/issue-to-indent/issue-to-indent-resolver.ts
@@ -1,15 +1,21 @@
-import { Injectable } from "@angular/core";
+import { Injectable, Inject } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
-import { BomService } from '../../services/bom/bom.service';
+import { BomService } from "../../services/bom/bom.service";
+import { MAT_DIALOG_DATA } from "@angular/material";
 
 @Injectable()
 export class IssueToIndentResolver implements Resolve<any> {
-    constructor(private bomService: BomService) { }
+  constructor(
+    private bomService: BomService,
+    @Inject(MAT_DIALOG_DATA) public data
+  ) {}
 
-    resolve() {
-        return this.bomService.getIssueToIndent(13, 4).then(data => {
-            console.log("asdfgh", data.data);
-            return data.data;
-        });
-    }
+  resolve() {
+    return this.bomService
+      .getIssueToIndent(this.data.materialId, this.data.projectId)
+      .then(data => {
+        console.log("asdfgh", data.data);
+        return data.data;
+      });
+  }
 }
