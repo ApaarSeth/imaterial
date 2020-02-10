fbdfe149b80db7e46b6547fee1b47dbd95858b14
subham.saha@buildsupply.com <subham.saha@buildsupply.com>
2020-02-10 19:21:39 +0530

    indent get lost Signed-off-by: subham.saha@buildsupply.com <subham.saha@buildsupply.com>

--- src/app/features/users/user-dashboard-routing.module.ts
+++ src/app/features/users/user-dashboard-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
-import { UserResolver } from './resolver/user.resolver';
 import { UserDetailComponent } from './user-details/user-details.component';
 const routes: Routes = [
   {
     path: "user-detail",
-    resolve: { indentList: UserResolver },
     component: UserDetailComponent
   }
 ];
