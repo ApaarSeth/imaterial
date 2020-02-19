e2b7b192ad8650bfe60064c6ee5b5bd629f11a70
subham.saha@buildsupply.com <subham.saha@buildsupply.com>
2020-02-19 18:21:46 +0530

    View-Rfq added for Non Submitted, add address supplier implemented, routing fixed! Signed-off-by: subham.saha@buildsupply.com <subham.saha@buildsupply.com>

--- src/app/features/rfq-supplier-template/rfq-supplier-detail-template-routing.module.ts
+++ src/app/features/rfq-supplier-template/rfq-supplier-detail-template-routing.module.ts
@@ -3,15 +3,25 @@ import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { RFQSupplierDetailComponent } from "./rfq-supplier-detail/rfq-supplier-detail.component";
 import { RFQSupplierAfterBidComponent } from './rfq-supplier-after-bid/rfq-supplier-after-bid.component';
+import { RFQSupplierAddAddressComponent } from './rfq-supplier-add-address/rfq-supplier-add-address.component';
 const routes: Routes = [
   {
     path: "supplier/:rfqId/:supplierId",
     component: RFQSupplierDetailComponent,
   },
     {
-    path: "after-submit/:brandList/:MaterialList",
+    path: "finish/:brandList/:MaterialList",
     component: RFQSupplierAfterBidComponent,
+  },
+  {
+    path: "finish",
+    component: RFQSupplierAfterBidComponent,
+  },
+  {
+    path: "add-address/:brandList/:MaterialList",
+    component:  RFQSupplierAddAddressComponent,
   }
+
 ];
 
 @NgModule({
--- src/app/shared/services/po/po.service.ts
+++ src/app/shared/services/po/po.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { DataService } from "../data.service";
 import { API } from "../../constants/configuration-constants";
-import { initiatePo } from "../../models/PO/po-data";
+import { initiatePo, SupplierAddress } from "../../models/PO/po-data";
+import { AllSupplierDetails } from '../../models/supplier';
 
 @Injectable({
   providedIn: "root"
@@ -36,5 +37,15 @@ export class POService {
 
   approveRejectPo(poStatusData) {
     return this.dataService.sendPostRequest(API.APPROVEREJECTPO, poStatusData);
+  }
+
+  getSupplierAddress(supplierId:Number){
+     return this.dataService.getRequest(
+      API.GETSUPPLIERADDRESS(supplierId)
+    );
+  }
+
+  addAddress(address : SupplierAddress){
+       return this.dataService.sendPostRequest(API.ADDSUPPLIERADDRESS, address);
   }
 }
