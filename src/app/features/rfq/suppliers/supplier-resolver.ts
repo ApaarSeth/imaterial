b712ff977ab05df152bba1a41f5d4f6fb3181bbb
Priya Garg <priya.garg@buildsupply.com>
2020-02-21 12:07:17 +0530

    material brand added in view grn

--- src/app/features/rfq/suppliers/supplier-resolver.ts
+++ src/app/features/rfq/suppliers/supplier-resolver.ts
@@ -1,15 +1,19 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
+import { Resolve } from "@angular/router";
 import { RFQService } from "src/app/shared/services/rfq/rfq.service";
 
 @Injectable()
+
 export class RFQSuppliersResolver implements Resolve<any> {
-  constructor(private rfqService: RFQService) {}
+
+  constructor(private rfqService: RFQService) { }
 
   resolve() {
-    let orgId= Number(localStorage.getItem('orgId'))
+    let orgId = Number(localStorage.getItem('orgId'))
     return this.rfqService.getSuppliers(orgId).then(data => {
       return data.data;
+      debugger
     });
   }
+
 }
