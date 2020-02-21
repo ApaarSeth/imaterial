6aef25d53deb192f89b8f7e9d78ed7777b5d5532
Priya Garg <priya.garg@buildsupply.com>
2020-02-21 11:33:22 +0530

    projectwise and materialwise API seperate

--- src/app/features/global-store/global-store.component.ts
+++ src/app/features/global-store/global-store.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRouteSnapshot, ActivatedRoute, Router } from "@angular/router";
+import { GlobalStoreService } from 'src/app/shared/services/global-store/global-store.service';
+import { GlobalProject } from 'src/app/shared/models/GlobalStore/projectWise';
 
 @Component({
   selector: "app-global-store",
@@ -7,21 +9,37 @@ import { ActivatedRouteSnapshot, ActivatedRoute, Router } from "@angular/router"
   styleUrls: ["../../../assets/scss/main.scss"]
 })
 export class GlobalStoreComponent implements OnInit {
+  
   buttonName: string = "materialWise";
   globalStoreData: [];
+  projectWiseData: GlobalProject[] = [];
+
   constructor(private route: ActivatedRoute,
-  private router: Router) {}
+    private router: Router,
+    private globalStoreService: GlobalStoreService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      console.log(data.globalData);
-      this.globalStoreData = data.globalData;
+      this.globalStoreData = data.globalData.data;
     });
   }
+
   setButtonName(name: string) {
+
     this.buttonName = name;
+
+    if (this.buttonName === 'projectWise') {
+
+      const orgId = Number(localStorage.getItem("orgId"));
+
+      this.globalStoreService.getProjectWiseData(orgId).then(res => {
+        this.projectWiseData = res.data;
+      })
+
+    }
   }
-  raiseRfq(){
+
+  raiseRfq() {
     this.router.navigate(['rfq/project-materials']);
   }
 }
