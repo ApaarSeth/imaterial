e31141ee61157ece43156bc60d2da170c7c5c5f7
subham.saha@buildsupply.com <subham.saha@buildsupply.com>
2020-03-25 20:55:41 +0530

    turn over condition checked Signed-off-by: subham.saha@buildsupply.com <subham.saha@buildsupply.com>

--- src/app/features/first-login/update-info/update-info.component.ts
+++ src/app/features/first-login/update-info/update-info.component.ts
@@ -67,6 +67,11 @@ export class UpdateInfoComponent implements OnInit {
     this._userService.getUserInfo(userId).then(res => {
       this.users = res.data ? res.data[0] : null;
       this.formInit();
+      if(this.users.roleName === 'l1')
+       {
+           this.userInfoForm.controls.turnOverId.setValidators([Validators.required]);
+         this.userInfoForm.controls.turnOverId.updateValueAndValidity();
+       }
     });
   }
   getTurnOverList() {
@@ -90,7 +95,7 @@ export class UpdateInfoComponent implements OnInit {
       email: [this.users ? this.users.email : '', Validators.required],
       contactNo: [this.users ? this.users.contactNo : '', Validators.required],
       roleId: [this.users ? this.users.roleId : null, Validators.required],
-      turnOverId: [this.users ? this.users.TurnOverId : null, Validators.required],
+      turnOverId: [this.users ? this.users.TurnOverId : null],
       userId: [this.users ? this.users.userId : null],
       ssoId: [this.users ? this.users.ssoId : null],
       country: ['India'],
