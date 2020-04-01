db5d86ee4c0d53cdaede98de9ba78e6dcdaed5de
Apaar <apaar.seth@buildsupply.com>
2020-04-01 16:59:25 +0530

    changes

--- src/app/shared/models/permissionObject.ts
+++ src/app/shared/models/permissionObject.ts
@@ -1,9 +1,11 @@
 export interface permission {
-    projectStoreFlag: boolean;
-    globalStoreFlag: boolean;
-    rfqFlag: boolean;
-    purchaseOrderFlag: boolean;
-    usersFlag: boolean;
-    supplierFlag: boolean;
-    projectEdit: boolean;
+    projectStoreFlag?: boolean;
+    globalStoreFlag?: boolean;
+    rfqFlag?: boolean;
+    purchaseOrderFlag?: boolean;
+    usersFlag?: boolean;
+    supplierFlag?: boolean;
+    projectEdit?: boolean;
+    addProject?: boolean
+    poApprovalFlag?: boolean
 }
