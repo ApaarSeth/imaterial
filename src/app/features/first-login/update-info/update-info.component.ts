a66dcebea61e97263a36c010a769c776a8cbc5e0
subham.saha@buildsupply.com <subham.saha@buildsupply.com>
2020-04-07 13:46:03 +0530

    File Size Implemented in Images Signed-off-by: subham.saha@buildsupply.com <subham.saha@buildsupply.com>

--- src/app/features/first-login/update-info/update-info.component.ts
+++ src/app/features/first-login/update-info/update-info.component.ts
@@ -40,6 +40,8 @@ export class UpdateInfoComponent implements OnInit {
   selectedTradesId: number[] = [];
   url: string;
   OthersId: number;
+  imageFileSizeError: string;
+  imageFileSize: boolean = false;
 
   constructor(private _userService: UserService,
     private _formBuilder: FormBuilder,
@@ -161,7 +163,16 @@ export class UpdateInfoComponent implements OnInit {
         this.localImg = (<FileReader>event.target).result;
       }
       const file = event.target.files[0];
-      this.uploadImage(file);
+        var fileSize =  event.target.files[0].size; // in bytes
+       if(fileSize < 2000000){
+        this.imageFileSizeError = "";
+        this.imageFileSize = true;
+        this.uploadImage(file);
+      }
+      else{
+         this.imageFileSize = false;
+         this.imageFileSizeError = "Image must be less than 2 mb";
+      }
     }
   }
 
