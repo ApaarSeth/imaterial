d4db32435821c1ad8dd76ca3402f9a2ccc90086e
Apaar <apaar.seth@buildsupply.com>
2020-04-07 12:37:40 +0530

    bom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,7 @@ const routes: Routes = [
   {
     path: "",
     component: ProfileLayoutComponent,
-    // canActivate: [AuthGuardService, UserDataGuardService],
+    canActivate: [AuthGuardService, UserDataGuardService],
     children: [
       {
         path: "profile",
