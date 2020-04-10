98f2e58a0d9536d3989fa6313b2bac11cc51b92f
subham.saha@buildsupply.com <subham.saha@buildsupply.com>
2020-04-10 12:58:16 +0530

    Upload File Size Done. Signed-off-by: subham.saha@buildsupply.com <subham.saha@buildsupply.com>

--- src/app/shared/dialogs/add-project/add-project.component.ts
+++ src/app/shared/dialogs/add-project/add-project.component.ts
@@ -48,6 +48,8 @@ export class AddProjectComponent implements OnInit {
   state: string;
   validPincode: boolean = null;
   pincodeLength: number;
+  imageFileSizeError: string = "";
+  imageFileSize: boolean = false;
 
   constructor(
     private projectService: ProjectService,
@@ -292,7 +294,18 @@ export class AddProjectComponent implements OnInit {
         this.localImg = (<FileReader>event.target).result;
       }
       const file = event.target.files[0];
-      this.uploadImage(file);
+       var fileSize =  event.target.files[0].size; // in bytes
+      if(fileSize < 1000000){
+        this.imageFileSizeError = "";
+        this.imageFileSize = true;
+        this.uploadImage(file);
+      }
+      else{
+         this.imageFileSize = false;
+         this.imageFileSizeError = "Image must be less than 1 mb";
+      }
+
+     // this.uploadImage(file);
     }
   }
 
