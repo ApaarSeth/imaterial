b3b8e0c38a6ef9470c5919b0ddf6bb6bc316d76f
Himanshu Aggarwal <himanshu.aggarwal@buildsupply.com>
2020-05-06 09:40:00 +0000

    Update envsetup.sh

--- buildScripts/envsetup.sh
+++ buildScripts/envsetup.sh
@@ -1,5 +1,7 @@
 set -x
 
 echo "" > /usr/share/nginx/html/assets/env.js
-echo "(function(window) {window.env.api_url = '"$BUILDSUPPLY_IM_BASE_API_URL"'" >> /usr/share/nginx/html/assets/env.js
+echo "(function(window) {window.env = {}; window.env.api_url = '"$BUILDSUPPLY_IM_BASE_API_URL"'" >> /usr/share/nginx/html/assets/env.js
 echo "})(this);"  >> /usr/share/nginx/html/assets/env.js
+
+
