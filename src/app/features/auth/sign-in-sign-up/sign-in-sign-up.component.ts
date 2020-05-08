61ffff55b26a74bc7c9a99b1b179d423d227da48
Nikhil Gupta <nikhil.gupta@buildsupply.com>
2020-05-08 14:47:37 +0000

    Update sign-in-sign-up.component.ts

--- src/app/features/auth/sign-in-sign-up/sign-in-sign-up.component.ts
+++ src/app/features/auth/sign-in-sign-up/sign-in-sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, Input } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { SignupComponent } from '../signup/signup.component';
+import { FacebookPixelService } from "../../../shared/services/fb-pixel.service";
 
 export interface OrganisationType {
   value: string;
@@ -20,13 +21,17 @@ export class SignInSignUpComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
+    private fbPixel: FacebookPixelService
   ) { }
 
   ngOnInit() {
+    this.fbPixel.load(); 
+    this.fbPixel.fire('PageView');
     if (localStorage.getItem("accessToken")) {
       this.router.navigate(['/dashboard'])
     }
+    
     this._activatedRoute.params.subscribe(param => {
       this.uniqueCode = param["uniqueCode"];
       this.index = this.uniqueCode ? 1 : 0;
