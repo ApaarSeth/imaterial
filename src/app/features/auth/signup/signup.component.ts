175107634e3a5bf6168d1fd91aa0036304c9d0c8
Apaar <apaar.seth@buildsupply.com>
2020-06-23 15:16:33 +0530

    currency changes

--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -85,8 +85,8 @@ export class SignupComponent implements OnInit {
   }
 
   ngOnChanges(): void {
-    // this.callingCode = this.actualCallingCode
-    this.callingCode = '+51'
+    this.callingCode = this.actualCallingCode
+    // this.callingCode = '+51'
     console.log("callingCode", this.callingCode)
     if (this.callingCode) {
       this.getLocation();
