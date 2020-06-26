c3e8d8b0faadb9f3fc6d2083424c4bbccf429328
Apaar <apaar.seth@buildsupply.com>
2020-06-26 17:44:47 +0530

    currency changes

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -181,7 +181,7 @@ export class ProfileComponent implements OnInit {
       email: [{ value: this.users ? this.users.email : '', disabled: true }, Validators.required],
       contactNo: [{ value: this.users ? this.users.contactNo : '', disabled: true }, Validators.required],
       roleId: [{ value: this.users ? this.users.roleId : null, disabled: false }, Validators.required],
-      turnOverId: [{ value: this.users ? this.users.TurnOverId : null, disabled: false }, Validators.required],
+      turnOverId: [{ value: this.users ? this.users.TurnOverId : null, disabled: false }],
       roleDescription: [{ value: this.users ? this.users.roleDescription : null, disabled: false }],
       userId: [this.users ? this.users.userId : null],
       ssoId: [this.users ? this.users.ssoId : null],
