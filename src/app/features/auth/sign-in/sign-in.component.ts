0397a9b4f6c576d5c334370fed96e5922ec3b00c
Apaar <apaar.seth@buildsupply.com>
2020-07-09 15:16:14 +0530

    ip address changes

--- src/app/features/auth/sign-in/sign-in.component.ts
+++ src/app/features/auth/sign-in/sign-in.component.ts
@@ -156,14 +156,7 @@ export class SigninComponent implements OnInit {
   }
 
   subscribeNotification() {
-    this.webNotificationService.subscribeToNotification()
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.available.subscribe(() => {
-        if (confirm("New version available. Load New Version?")) {
-          window.location.reload();
-        }
-      });
-    }
+    this.webNotificationService.subscribeToNotification();
     this.swPush.notificationClicks.subscribe(({ action, notification }) => {
       window.open(notification.data.url)
     })
