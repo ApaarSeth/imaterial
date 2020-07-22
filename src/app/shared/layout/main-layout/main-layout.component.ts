11b026002ea5c8748acf6fd4e50e9ecf953c4746
rohitsharma <rohit.sharma@buildsupply.com>
2020-07-22 21:06:26 +0530

    subscriptions

--- src/app/shared/layout/main-layout/main-layout.component.ts
+++ src/app/shared/layout/main-layout/main-layout.component.ts
@@ -17,6 +17,8 @@ export class MainLayoutComponent implements OnInit {
   url: string;
   isSideNavCollapsed: boolean;
   menuData: MenuList;
+  subscriptionsData: any;
+  isFreeTrialActivate: boolean;
 
   constructor(private router: Router,
     private _userService: UserService,
@@ -26,10 +28,29 @@ export class MainLayoutComponent implements OnInit {
   loaded = '';
   ngOnInit() {
     this.menuData = this.activateRoute.snapshot.data.menu;
+    this.subscriptionsData = this.activateRoute.snapshot.data.subsData;
     this.userId = Number(localStorage.getItem("userId"));
     this.userName = localStorage.getItem("userName");
     this.url = localStorage.getItem('profileUrl');
     this.startSubscriptions();
+    this.checkFreeTrial();
+  }
+
+  checkFreeTrial() {
+    const data = this.subscriptionsData;
+    if (data && data.planFrequencyList.length) {
+      let checked = 0;
+      for (let i = 0; i < data.planFrequencyList.length; i++) {
+        for (let x = 0; x < data.planFrequencyList[ i ].planList.length; x++) {
+          if (checked == 0) {
+            if (data.planFrequencyList[ i ].planList[ x ].isTrialActive === 1) {
+              checked = 1;
+              this.isFreeTrialActivate = true;
+            }
+          }
+        }
+      }
+    }
   }
 
 
