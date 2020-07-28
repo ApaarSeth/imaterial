b57b99492d4b8f6a464ead19221ed2741fa25eb8
rohitsharma <rohit.sharma@buildsupply.com>
2020-07-28 11:20:10 +0530

    rfq details

---
+++ src/app/shared/components/advance-search/advance-search.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { CommonService } from '../../services/commonService';
+import { ProjectService } from '../../services/projectDashboard/project.service';
+import { Projects } from '../../models/GlobalStore/materialWise';
+
+@Component({
+    selector: 'advance-search',
+    templateUrl: './advance-search.component.html'
+})
+
+export class AdvanceSearchComponent implements OnInit {
+
+    @Input('filterType') filterType: string;
+    userId: number;
+    orgId: number;
+    proejectsData: any;
+
+    constructor(
+        private commonService: CommonService,
+        private projectService: ProjectService
+    ) { }
+
+    ngOnInit(): void {
+        console.log(this.filterType);
+        this.orgId = Number(localStorage.getItem('orgId'));
+        this.userId = Number(localStorage.getItem('userId'));
+    }
+
+    displayWith(option) {
+        return option && option.projectName ? option.projectName : ''
+    }
+
+    searchFilter(searchValue: string, type: string) {
+        const value = searchValue;
+        if (type === 'projects') {
+            if (!this.proejectsData) {
+                this.getProjects();
+            }
+        }
+    }
+
+    getProjects() {
+        this.projectService.getProjects(this.orgId, this.userId, false).then(res => {
+            if (res.data.length) {
+                this.proejectsData = res.data;
+                console.log(JSON.stringify(this.proejectsData));
+            }
+        });
+    }
+
+}
