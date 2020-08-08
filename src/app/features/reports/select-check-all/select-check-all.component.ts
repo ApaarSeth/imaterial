4c9526457e1e61ae92d5f841ff07f935601038a4
Apaar <apaar.seth@buildsupply.com>
2020-08-08 11:20:54 +0530

    update info changes

---
+++ src/app/features/reports/select-check-all/select-check-all.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material';
+
+@Component({
+    selector: 'app-select-check-all',
+    templateUrl: "./select-check-all.component.html"
+})
+
+export class SelectCheckAllComponent {
+    @Input() model: FormControl;
+    @Input() values = [];
+    @Input() text = 'Select All';
+    @Output() selectAllText = new EventEmitter<string>();
+    
+    ngOnInit(): void {}
+
+    isChecked(): boolean {
+        return this.model.value && this.values.length
+            && this.model.value.length === this.values.length;
+    }
+
+    toggleSelection(change: MatCheckboxChange): void {
+        if (change.checked) {
+            this.model.setValue(this.values);
+            this.selectAllText.emit(this.text);
+        } else {
+            this.model.setValue([]);
+            this.selectAllText.emit('');
+        }
+    }
+}
