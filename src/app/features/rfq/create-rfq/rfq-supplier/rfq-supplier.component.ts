9c453ee59c0407b2acee796b32d47844a672d5c7
Apaar <apaar.seth@buildsupply.com>
2020-08-24 15:57:15 +0530

    changes

--- src/app/features/rfq/create-rfq/rfq-supplier/rfq-supplier.component.ts
+++ src/app/features/rfq/create-rfq/rfq-supplier/rfq-supplier.component.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs/operators';
 import { Component, OnInit, Input, SimpleChanges, Output, EventEmitter } from "@angular/core";
 import { Suppliers } from "src/app/shared/models/RFQ/suppliers";
 import {
@@ -141,16 +142,11 @@ export class RfqSupplierComponent implements OnInit {
   }
 
   supplierAdded() {
-    this.rfqData.supplierId = this.supplierForm.value.forms.map(supplier => {
-      if (supplier.supplier) {
-        return supplier.supplier.supplierId;
-      }
-    });
-    this.rfqData.supplierId = this.rfqData.supplierId.filter(supplierId => {
-      if (supplierId) {
-        return supplierId;
-      }
-    });
+    this.rfqData.supplierId = this.supplierForm.value.forms.filter(supplier => {
+      return supplier.supplier != null
+    }).map(supplier => {
+      return supplier.supplier.supplierId;
+    })
   }
 
   changeRfq() {
