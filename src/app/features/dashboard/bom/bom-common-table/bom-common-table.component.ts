5f6e445b42a79985035b6ca7d4f8c0b49675f8cb
rohitsharma <rohit.sharma@buildsupply.com>
2020-09-22 17:28:24 +0530

    bugs fixes

--- src/app/features/dashboard/bom/bom-common-table/bom-common-table.component.ts
+++ src/app/features/dashboard/bom/bom-common-table/bom-common-table.component.ts
@@ -149,12 +149,4 @@ export class BomCommonTableComponent implements OnInit, OnChanges {
         }
     }
 
-    // isAlphaNumeric(e) {
-    //     var x = e.which || e.keycode;
-    //     if ((x >= 48 && x <= 57) || x == 46)
-    //         return true;
-    //     else
-    //         return false;
-    // }
-
 }
